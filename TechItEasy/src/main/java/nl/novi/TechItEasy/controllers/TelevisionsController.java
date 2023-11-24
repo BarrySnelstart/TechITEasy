ce555cfaf0d4acc309dee59e7674d63e4f0abe86
Barry Kramer <bkramer@snelstart.nl>
2023-11-24 11:58:29 +0100

    Added PostMapping

--- TechItEasy/src/main/java/nl/novi/TechItEasy/controllers/TelevisionsController.java
+++ TechItEasy/src/main/java/nl/novi/TechItEasy/controllers/TelevisionsController.java
@@ -3,89 +3,32 @@ package nl.novi.TechItEasy.controllers;
 import nl.novi.TechItEasy.exceptions.NameToLongException;
 import nl.novi.TechItEasy.exceptions.RecordNotFoundException;
 import nl.novi.TechItEasy.models.Television;
+import nl.novi.TechItEasy.repositories.TelevisionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/television")
 public class TelevisionsController {
-    int id = 1000;
-        List <Television> televisionDataBase = new ArrayList <>();
-
-
-//    @PostMapping()
-//    public ResponseEntity <Television> addTvToList(@RequestBody Television television) {
-//        television.setID(id);
-//        id ++;
-//        if(television.getName().length() >  20)
-//        {
-//            throw new NameToLongException("Name is to long");
-//        }
-//        this.televisionDataBase.add(television);
-//
-//        return new  ResponseEntity<>(television,HttpStatus.CREATED);
-//    }
-
-    // ------------------------------------------------------------------------
-//    @PutMapping("/{ID}")
-//    public ResponseEntity<String> changeTvModelName(@PathVariable int ID, @RequestBody String model) {
-//
-//        for (Television tv:televisionDataBase) {
-//            if(tv.getID() == ID)
-//            {
-//                String oldModel = tv.getModel();
-//                tv.setModel(model);
-//                return ResponseEntity.ok("Tv found changed model:"+ oldModel + "To : " + tv.getModel());
-//
-//            }
-//        }
-//        throw new RecordNotFoundException("Tv not found");
-//    }
-
-    // ------------------------------------------------------------------------
-    @GetMapping()
-    public ResponseEntity <List<Television>> getAllTelevisions() {
-        return new ResponseEntity<>(this.televisionDataBase, HttpStatus.OK);
+    @Autowired private TelevisionRepository televisionRepository;
+      List <Television> televisionDataBase = new ArrayList <>();
+    @GetMapping("televisions/{id}")
+    public ResponseEntity<Television> getTvById(@PathVariable("id")  long id) {
+        Optional<Television> televisionSaved = televisionRepository.findById(id);
+        return ResponseEntity.ok(televisionSaved.get());
     }
 
-//    @GetMapping("/{ID}")
-//    public ResponseEntity <String> getTvByID(@PathVariable int ID) {
-//
-//        for (Television tv:televisionDataBase) {
-//            if(tv.getID() == ID)
-//            {
-//                return ResponseEntity.ok("Tv found " + tv.getName() +" " +tv.getModel());
-//            }
-//        }
-//        throw new RecordNotFoundException("Tv not found");
-//    }
+    @PostMapping
+    public ResponseEntity<Television> addTvToDataBase (@RequestBody Television television){
+    Television televisionSaved = televisionRepository.save(television);
+        return ResponseEntity.created(null).body(televisionSaved);
+    }
 
-//    // ------------------------------------------------------------------------
-//    @DeleteMapping("/{ID}")
-//    public ResponseEntity deleteTv(@PathVariable int ID) {
-//    List<Television> deleteList = new ArrayList<>();
-//        for (Television tv:televisionDataBase) {
-//            if(tv.getID() == ID)
-//            {
-//                deleteList.add(tv);
-//            }
-//
-//        }
-//        if(!deleteList.isEmpty())
-//        {
-//            for(Television delete: deleteList)
-//            {
-//                televisionDataBase.remove(delete);
-//            }
-//        }
-//
-//        return ResponseEntity.ok("Deleted");
-//
-//
-//    }
 
 }
--- TechItEasy/src/main/java/nl/novi/TechItEasy/models/Television.java
+++ TechItEasy/src/main/java/nl/novi/TechItEasy/models/Television.java
@@ -1,7 +1,20 @@
 package nl.novi.TechItEasy.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+@Entity
+@Table(name="televisions")
+@Getter
+@Setter
 public class Television {
-   private Long id;
+    @Id
+    @GeneratedValue
+
+    private Long id;
     private String type;
     private String brand;
     private String name;
@@ -19,24 +32,31 @@ public class Television {
     private Integer originalStock;
     private Integer sold;
 
+    public Television (){
 
-            public enum availableSize {
+    }
+    public Television(Long id, String type, String brand, String name, Double price, Double availableSize, Double refreshRate, String screenType, String screenQuality, Boolean smartTv, Boolean wifi, Boolean voiceControl, Boolean hdr, Boolean bluetooth, Boolean ambiLight, Integer originalStock, Integer sold) {
+        this.id = id;
+        this.type = type;
+        this.brand = brand;
+        this.name = name;
+        this.price = price;
+        this.availableSize = availableSize;
+        this.refreshRate = refreshRate;
+        this.screenType = screenType;
+        this.screenQuality = screenQuality;
+        this.smartTv = smartTv;
+        this.wifi = wifi;
+        this.voiceControl = voiceControl;
+        this.hdr = hdr;
+        this.bluetooth = bluetooth;
+        this.ambiLight = ambiLight;
+        this.originalStock = originalStock;
+        this.sold = sold;
+    }
 
-            }
-            public enum refreshRate {
 
-            }
-            public enum screenType {
 
-            }
-
-            public enum screenQuality {
-
-            }
-
-//    default constructor
-//            constructor
-//    alle getters en setters
 
 
 
