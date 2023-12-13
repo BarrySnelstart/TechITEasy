a0db102ed629265b8936754d46717a17ccf44bff
Barry Kramer <bkramer@snelstart.nl>
2023-12-13 11:35:15 +0100

    Added Controllers

---
+++ TechItEasy/src/main/java/nl/novi/TechItEasy/controllers/CIModuleController.java
@@ -0,0 +1,9 @@
+package nl.novi.TechItEasy.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/cimodules")
+@RestController
+public class CIModuleController {
+}
---
+++ TechItEasy/src/main/java/nl/novi/TechItEasy/controllers/RemoteControllerController.java
@@ -0,0 +1,10 @@
+package nl.novi.TechItEasy.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/remotecontrollers")
+@RestController
+public class RemoteControllerController {
+
+}
